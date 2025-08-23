b9e08fe313e44ab721d109169818d1b476eaa48b
Albert Martinez <ialbertmartinez@outlook.com>
2025-08-23 01:56:13 -0700

    add React import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+
 function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
